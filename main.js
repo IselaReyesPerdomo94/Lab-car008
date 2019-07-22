793792dfbc19c07ccc9bc3e66421def2f6dd3a60
Isela Reyes Perdomo <reyesp.isela@gmail.com>
2019-07-21 21:53:46 -0500

    Agregando modal de iniciar sesión estilizado, tanto para mobile como para desktop

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Que esta pasando?')
+//Getting buttons that trigger modal boxes
+
+const signInDesk = document.getElementById('signin-desk');
+const signIn = document.getElementById('signin');
+const closeSignin = document.getElementById('close-signin');
+
+//functions for showing or hidding modal boxes
+const showModal = id => document.getElementById(id).classList.remove('hide');
+const hideModal = id => document.getElementById(id).classList.add('hide');
+
+//show sign in modal
+const openSignInModal = () => showModal('signin-modal');
+
+const closeSignInModal = () => hideModal('signin-modal');
+
+signIn.addEventListener('click', openSignInModal);
+signInDesk.addEventListener('click', openSignInModal);
+closeSignin.addEventListener('click', closeSignInModal);
